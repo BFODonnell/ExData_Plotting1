e0445d608b535658721fc9815079013987fc1e65
Brian <brian_frank_odonnell@yahoo.com>
2016-02-08 00:10:46 -0500

    Line plot color change.
    Changed color of sub metering 1 to black.

--- plot4.R
+++ plot4.R
@@ -18,7 +18,7 @@ plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (
 plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 
 # Lower left quadrant
-plot(datetime, sub_metering_1, col="green", type="l", xlab="", ylab="Energy sub metering")
+plot(datetime, sub_metering_1, col="black", type="l", xlab="", ylab="Energy sub metering")
 lines(datetime, sub_metering_2, col="red", type="l")
 lines(datetime, sub_metering_3, col="blue", type="l")
 legend("topright", c("Sub_metering_1","Sub_metering_1","Sub_metering_3"), col=c("black", "red", "blue"), lwd=2.5)
